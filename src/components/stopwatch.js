0cf9839cc9133a299a734b9b9d1b4fc28879317e
Davideuijinlee <Davideuijinlee@gmail.com>
2019-03-28 20:05:34 -0700

    stopwatch not complete revist

---
+++ src/components/stopwatch.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import Time from './format_time';
+
+class Stopwatch extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            'status': 'stopped',
+            'start': null,
+            'elapsed': 0
+        }
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+    }
+    update=()=>{
+        const {status, start} = this.state;
+        if(status === 'running'){
+            this.setState({
+                'elapsed': new Date().getTime() - start
+            });
+            setTimeout(this.update, 10);
+        }
+    }
+    start(){
+        this.setState({
+            'status': 'running',
+            'start': new Date().getTime()
+        });
+        setTimeout(this.update, 10);
+    }
+    stop(){
+        this.setState({
+            'status': 'stopped'
+        })
+    }
+    reset=()=>{
+        this.setState({
+            'status': 'stopped',
+            'start': null,
+            'elapsed': 0
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1><Time elapsed={this.state.elapsed}/></h1>
+                <p>{this.state.status}</p>
+                <button onClick={this.start}>Start</button>
+                <button onClick={this.stop}>Stop</button>
+                <button onClick={this.reset}>Reset</button>
+            </div>
+        )
+    }
+}
+
+export default Stopwatch;
