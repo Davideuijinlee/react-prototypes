ed5039f41ae2a1f2ce60532f1d51e7257fd3beba
Davideuijinlee <Davideuijinlee@gmail.com>
2019-03-28 18:38:50 -0700

    functional components - David Lee

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table';
 
+export default () => {
+    const students = [
+        {
+            student: 'Chris', 
+            course: 'Crime Fighting', 
+            grade: '82'},
+        {
+            student: 'jason', 
+            course: 'blowing bubbles', 
+            grade: '99'
+        },
+        {
+            student: 'Jaime', 
+            course: 'origami making', 
+            grade: '50'}
+    ];
+
+    return (
+    <div className="container">
+        <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+        </div>    
+    </div>
+    )
+}
 
